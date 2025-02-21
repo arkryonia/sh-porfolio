4ce2eedd8969bef2a9ec19faeaa19477a5e13c3d
Hodonou Sounton <sounton@gmail.com>
2025-02-21 19:12:32 +0100

    ✨ In progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import About from './ui/components/About';
+import Hero from './ui/components/Hero';
+import Skills from './ui/components/Skills';
+
 const HomePage = () => {
-  return <div className="text-8xl">HomePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Hero />
+      <About />
+      <Skills />
+
+      <div id="projects" className="flex flex-col items-center justify-center w-full h-screen">
+        {/* Projects section */}
+        <h2 className="text-3xl font-bold">Projects</h2>
+      </div>
+      {/* Testimonial section */}
+      {/* Contact section */}
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/ui/components/About.tsx
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div
+      id="about"
+      className="flex flex-col items-center justify-center w-full min-h-screen px-4 py-8"
+    >
+      <h2 className="text-3xl font-bold mb-8">About</h2>
+      <div className="w-full max-w-3xl space-y-8">
+        <p className="text-lg md:text-xl text-justify">
+          As an Information Systems Engineer and a passionate advocate for open-source technologies,
+          I thrive at the intersection of technology and culture. With a strong background in
+          developing applications using Java, Flutter and React, I lead a digital services company
+          dedicated to creating impactful solutions that cater to diverse user needs. My commitment
+          to the Debian project reflects my belief in the power of community-driven software, and I
+          am currently focused on developing a localized version of Debian for West Africa.
+        </p>
+        <p className="text-lg md:text-xl text-justify">
+          In addition to my technical expertise, I am pursuing a degree in Sociology and
+          Anthropology, where I explore the intricate relationship between traditional spiritual
+          practices and societal dynamics. This unique blend of skills allows me to approach
+          challenges with a data-driven mindset while incorporating cultural perspectives that drive
+          sustainable development.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
